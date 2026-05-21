f930a27a1e6d40fc69bbc2d88a95fd42f1f9ca63
Mimbao <chowjenghan@gmail.com>
2026-05-22 00:42:09 +0800

    added authentification

---
+++ Orbital-JH-Squared-main/src/pages/home.tsx
@@ -0,0 +1,25 @@
+
+import { useNavigate } from "react-router-dom";
+
+
+export default function Home() {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <h1>NUSMods Plus</h1>
+
+            <h2>Home Page</h2>
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+
