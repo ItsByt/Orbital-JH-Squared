1491cf81386e714c3013c388a8fe6361a94f7cb0
Mimbao <chowjenghan@gmail.com>
2026-06-05 10:55:36 +0800

    Bugs Fixed

--- src/utils/timetableUtils/lessonClashDetection.ts
+++ src/utils/timetableUtils/lessonClashDetection.ts
@@ -1,9 +1,8 @@
 import type { DisplayLesson } from "@/types";
 
 export function doLessonsSchedulesClash(l1: DisplayLesson, l2: DisplayLesson): boolean {
-    if (l1.classNo === "CUSTOM_IGNORE_FLAG" || l2.classNo === "CUSTOM_IGNORE_FLAG") {
-        return false;
-    }
+    if (l1.classNo === "CUSTOM_IGNORE_FLAG" || l2.classNo === "CUSTOM_IGNORE_FLAG") return false;
+    
     if (l1.day !== l2.day) return false;
 
     if ((l1.weekBitmask & l2.weekBitmask) === 0) return false;
